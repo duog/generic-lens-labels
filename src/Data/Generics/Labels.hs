2c2ed522e7ac9896624ba80dfdee03176f4708f5
Douglas Wilson <douglas.wilson@gmail.com>
2018-01-09 16:05:20 +1300

    First draft of a compatible Data.Generics.Labels

--- src/Data/Generics/Labels.hs
+++ src/Data/Generics/Labels.hs
@@ -20,10 +20,42 @@ _bar = #bar
 {-
 This is the instance head that seems to give the best type inference.
 -}
+
+#if MIN_VERSION_generic_lens(0,5,0)
+#if __GLASGOW_HASKELL__ < 802
+
 instance (HasField x s t a b, Functor f, lens ~ ((a -> f b) -> s -> f t)) =>
   IsLabel x lens where
-#if __GLASGOW_HASKELL__ >= 802
-  fromLabel = field @ x
-#else
   fromLabel _ = field @ x
+
+#else
+-- __GLASGOW_HASKELL
+
+instance (HasField x s t a b, Functor f, lens ~ ((a -> f b) -> s -> f t)) =>
+  IsLabel x lens where
+  fromLabel = field @ x
+
 #endif
+-- __GLASGOW_HASKELL
+
+#else
+-- MIN_VERSION_generic_lens
+
+#if __GLASGOW_HASKELL < 802
+
+instance (HasField x a s, Functor f, lens ~ ((a -> f a) -> s -> f s)) =>
+  IsLabel x lens where
+  fromLabel _ = field @ x
+
+#else
+-- __GLASGOW_HASKELL
+
+instance (HasField x a s, Functor f, lens ~ ((a -> f a) -> s -> f s)) =>
+  IsLabel x lens where
+  fromLabel = field @ x
+
+#endif
+-- __GLASGOW_HASKELL
+
+#endif
+-- MIN_VERSION_generic_lens
